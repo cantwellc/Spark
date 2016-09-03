a25d5f2edb3c73a3440c52424bcfb8ae5a145a76
Chris Cantwell <ccantwel@usc.edu>
2016-09-03 14:23:06 -0700

    Added components to fix position and rotation 2D XZ plane

---
+++ Assets/Scripts/LockY.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockY : MonoBehaviour {
+    public float LockPosition;
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, LockPosition, transform.position.z);
+    }
+}
