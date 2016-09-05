b04482bb662f594778d87d412ba50268bc81b69d
Goksucan Akin <Goksucan Akin>
2016-09-05 15:02:34 -0700

    Added volume adjustments to background music by pressing up and down arrow keys

---
+++ Assets/Scripts/Music/VolumeAdjustment.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeAdjustment : MonoBehaviour {
+	
+	public AudioSource musicObject;
+
+	// Use this for initialization
+	void Start () {
+		musicObject = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			if(musicObject.volume < 1.0f){
+				musicObject.volume += 0.1f;
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			if (musicObject.volume > 0.0f) {
+				musicObject.volume -=0.1f;
+			}
+		}
+	}
+}
