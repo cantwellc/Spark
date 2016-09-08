a24b61ffb108d893c3d4903fc1e85b63cf2d4e4b
Chris Cantwell <ccantwel@usc.edu>
2016-09-08 12:54:09 -0700

    Added Wormhole and Blackhole

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Gravity : MonoBehaviour {
+    public float Strength;
+    public GameObject Ship;
+
+	// Update is called once per frame
+	void Update () {
+        Ship.GetComponent<Rigidbody>().AddForce(CalcGravity());
+	}
+
+    private Vector3 CalcGravity()
+    {
+        Vector3 vec = Ship.transform.position - transform.position;
+        float r2 = vec.sqrMagnitude;
+        var norm = vec.normalized;
+        return - norm * Strength / r2;
+
+    }
+}
---
+++ Assets/Scripts/WormHole.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WormHole : MonoBehaviour {
+    public WormHole Exit;
+    public GameObject Ship;
+    public float Cooldown;
+
+    bool _triggerEnabled = true;
+
+    public void OnTriggerEnter()
+    {
+        if (!_triggerEnabled) return;
+        _triggerEnabled = false;
+        StartCoroutine(EnableTriggerAfterCooldown());
+        Exit.TakeShip();
+    }
+
+    public void TakeShip()
+    {
+        _triggerEnabled = false;
+        Ship.transform.position = transform.position;
+        StartCoroutine(EnableTriggerAfterCooldown());
+    }
+
+    private IEnumerator EnableTriggerAfterCooldown()
+    {
+        yield return new WaitForSeconds(Cooldown);
+        _triggerEnabled = true;
+        yield break;
+    }
+}
