f584c874b5c4c19c354686ffe5160477d6af7db2
Handa Zhang <handazh2@gmail.com>
2016-09-11 13:36:16 -0700

    Added some particle system to character bullet prefab
    Looks like shit though, would need some iteration

---
+++ Assets/Scripts/Character/Attack_Movement/Bullet/CharacterBulletSelfRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterBulletSelfRotation : MonoBehaviour {
+
+    public float rotationSpeed = 10;
+
+	void Update () {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+	}
+}
