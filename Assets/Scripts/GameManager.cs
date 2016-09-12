654d715843523f5b14b5a3939e60921f4a7d2291
Chunze <cchunze@yahoo.com>
2016-09-12 10:16:03 -0700

    Added GameManager, which will process all keyborad input and UI.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Text notificationText;
+    public Text fuelCountText;
+    public Character ship;
+
+    private FuelReservoir _fuelReservoir;
+
+    // Use this for initialization
+    void Start ()
+    {
+        notificationText.enabled = false;
+        _fuelReservoir = ship.GetComponent<FuelReservoir>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(CheatModeMassage(ship.ToggleCheatCode()));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        fuelCountText.text = _fuelReservoir.fuelCount + "/" + _fuelReservoir.maxFuelCount;
+    }
+
+    IEnumerator CheatModeMassage(bool CheatMode)
+    {
+        if (CheatMode)
+        {
+            notificationText.text = "Cheat Mode Enabled!";
+        }
+        else
+        {
+            notificationText.text = "Cheat Mode Disabled!";
+        }
+        notificationText.enabled = true;
+        notificationText.color = Color.red;
+        yield return new WaitForSeconds(1.2f);
+        notificationText.enabled = false;
+    }
+}
