8a8998ba6582ede14e010c7be46061664ba36c08
Handa Zhang <handazh2@gmail.com>
2016-09-14 15:14:09 -0700

    Minor change in bullet explosion effect

--- Assets/Scripts/Character/Attack_Movement/Bullet/CharacterBulletCollision.cs
+++ Assets/Scripts/Character/Attack_Movement/Bullet/CharacterBulletCollision.cs
@@ -13,10 +13,15 @@ public class CharacterBulletCollision : MonoBehaviour
 		{
 			Health enemyHealth = other.gameObject.GetComponent<Health> ();
 			enemyHealth.TakeDamage (_damage);
-			Destroy (gameObject);
+            GameObject explosion = (GameObject)Instantiate(plasmaExplosionPrefab, transform.position, Quaternion.Euler(-90f, 0, 0));
+            Destroy(explosion, 0.5f);
+            Destroy (gameObject);
 		}
 		else if (other.gameObject.tag != "Character" && other.gameObject.tag != "Wall") 
 		{
+
+            GameObject explosion = (GameObject)Instantiate(plasmaExplosionPrefab, transform.position, Quaternion.Euler(-90f, 0, 0));
+            Destroy(explosion, 0.5f);
             Destroy (gameObject);
 		}
 	}
@@ -27,7 +32,5 @@ public class CharacterBulletCollision : MonoBehaviour
 
     void OnDestroy()
     {
-        GameObject explosion = (GameObject)Instantiate(plasmaExplosionPrefab, transform.position, Quaternion.Euler(-90f, 0, 0));
-        Destroy(explosion, 0.5f);
     }
 }
