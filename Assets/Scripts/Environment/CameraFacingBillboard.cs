eb31d23cb49eea6aae34d74f8893212ad4d916eb
Chris Cantwell <ccantwel@usc.edu>
2016-09-16 20:11:52 -0700

    Added simple image distortion effect to BlackHole

---
+++ Assets/Scripts/Environment/CameraFacingBillboard.cs
@@ -0,0 +1,14 @@
+﻿// From https://unity3dt.wordpress.com/2015/08/28/interstellar-black-hole-gargantua-tutorial/
+
+using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    void Update()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+        m_Camera.transform.rotation * Vector3.up);
+    }
+}
