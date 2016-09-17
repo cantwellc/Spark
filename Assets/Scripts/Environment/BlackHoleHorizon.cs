a868678eeedae22adefd325233dcbe5e7f92548c
Chris Cantwell <ccantwel@usc.edu>
2016-09-16 20:57:25 -0700

    Implemented BlackHole scaling with mass, and increase mass on absorbing objects.

---
+++ Assets/Scripts/Environment/BlackHoleHorizon.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHoleHorizon : MonoBehaviour {
+    public Rigidbody blackHoleRigidBody;
+
+	void OnTriggerEnter(Collider other)
+    {
+        // Add mass and destroy other object
+        var otherMass = other.gameObject.GetComponent<Rigidbody>().mass;
+        blackHoleRigidBody.mass += otherMass;
+        other.gameObject.SetActive(false);
+    }
+}
