8035a17a9cddf7685acb7e69212c555c1a03e1ff
divyansb <divyansb@usc.edu>
2016-09-21 14:39:07 -0700

    Levels
    Made changes to the levels

---
+++ Assets/Scripts/Tutorial Text/Blackhole.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blackhole : MonoBehaviour {
+
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(!enemy)
+        {
+            GetComponent<TextMesh>().text = "BLACKHOLE!";
+            GetComponent<TextMesh>().color = Color.green;
+        }
+	}
+}
