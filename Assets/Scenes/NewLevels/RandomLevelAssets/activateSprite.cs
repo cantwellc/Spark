6bdc0f07afcc8caf0baacb2c25f3cdc85da513f7
divyansb <divyansb@usc.edu>
2016-09-24 03:16:24 -0700

    Levels 2 and 3
    Added levels 2 and 3

---
+++ Assets/Scenes/NewLevels/RandomLevelAssets/activateSprite.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activateSprite : MonoBehaviour {
+
+    public GameObject sprite;
+
+	void OnTriggerEnter(Collider Other)
+    {
+        if(Other.tag == "Character")
+            sprite.SetActive(true);
+    }
+}
