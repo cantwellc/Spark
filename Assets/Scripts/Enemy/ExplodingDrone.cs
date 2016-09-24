2457325008b21794a40df05c955d05fa951b359a
Handa Zhang <handazh2@gmail.com>
2016-09-24 12:49:04 -0700

    Added Drones moving

---
+++ Assets/Scripts/Enemy/ExplodingDrone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodingDrone : MonoBehaviour {
+
+    public float moveForce;
+
+    Rigidbody _rigidBody;
+	// Use this for initialization
+	void Start () {
+        _rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform targetTransform = Character.current.transform;
+
+        Vector3 dir = (targetTransform.position - transform.position).normalized;
+        _rigidBody.AddForce(dir * moveForce);
+
+        Quaternion dest = Quaternion.LookRotation(dir.normalized);
+        transform.rotation = Quaternion.Slerp(transform.rotation, dest, 1);
+    }
+    
+    void OnCollisionEnter()
+    {
+    }
+}
