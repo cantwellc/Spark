cdcacacef9c4eaf9d78c8b7938639cbab8087d75
cmenekse <canmenekse@sabanciuniv.edu>
2016-09-28 15:22:34 -0700

    Changed Fuel Drop Mechanic on the boss

--- Assets/DropFuel.cs
+++ Assets/DropFuel.cs
@@ -3,14 +3,27 @@ using System.Collections;
 
 public class DropFuel : MonoBehaviour {
 
+	private GameObject _fuelInstance;
+	private float _percentageToDropHealth;
+
 	public Transform bossTransform;
 	public GameObject fuel;
-	private GameObject _fuelInstance;
-	public void DropFuelCircular()
+
+	public Health bossHealth;
+	public float percentageIntervalToDropFuel;
+
+	void Start()
+	{
+		_percentageToDropHealth = bossHealth.HealthPercent *100 - percentageIntervalToDropFuel;
+	}
+
+
+	void Update()
 	{
 		
-		if (Random.Range (0.0f, 50.0f) <= 8.0f)
+		while (bossHealth.HealthPercent *100  <= _percentageToDropHealth)
 		{
+
 			float r = Random.Range (7f, 10.0f);
 			float cx = bossTransform.transform.position.x;
 			float cz = bossTransform.transform.position.z;
@@ -22,8 +35,8 @@ public class DropFuel : MonoBehaviour {
 			_fuelInstance = Instantiate (fuel, fuelPosition, transform.rotation) as GameObject;
 			_fuelInstance.GetComponent<Fuel> ().fuelAmmount = Random.Range (250, 350);
 			_fuelInstance.GetComponent<Fuel> ().ScaleFuel ();
+			_percentageToDropHealth -= percentageIntervalToDropFuel;
 		}
-
-
 	}
+
 }
