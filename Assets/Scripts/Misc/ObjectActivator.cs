348e114d4d9d1475be229f358990a8f22dddfbc2
Chris Cantwell <ccantwel@usc.edu>
2016-09-28 01:49:49 -0700

    Built Drone level, needs filling out.

---
+++ Assets/Scripts/Misc/ObjectActivator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectActivator : MonoBehaviour {
+    public GameObject target;
+	
+    public void Activate()
+    {
+        target.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Misc/ObjectDeactivator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDeactivator : MonoBehaviour {
+    public GameObject target;
+
+    public void Deactivate()
+    {
+        target.SetActive(false);
+    }
+}
