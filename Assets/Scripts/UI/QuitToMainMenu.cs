74508786ca5c2edfc96826c332209e6e9fe5874b
cmenekse <canmenekse@sabanciuniv.edu>
2016-09-28 01:10:31 -0700

    Fixed Scaling of some turrets in level 4

---
+++ Assets/Scripts/UI/QuitToMainMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class QuitToMainMenu : MonoBehaviour 
+{		
+		public void Quit()
+		{
+			#if UNITY_EDITOR
+
+			StartCoroutine(EditorQuit());
+
+			#else
+			StartCoroutine(ApplicationQuit());
+			#endif
+		}
+
+		IEnumerator EditorQuit()
+		{
+			if(Time.timeScale == 0)
+			{
+				Time.timeScale = 1;
+			}
+			float fadeTime = GameObject.Find("GameManager").GetComponent<SceneFader>().BeginFade(1);
+			yield return new WaitForSeconds (fadeTime);
+			SceneManager.LoadScene ("MainMenu");
+			
+			
+		}
+
+		IEnumerator ApplicationQuit()
+		{
+			if (Time.timeScale == 0)
+			{
+				Time.timeScale = 1;
+			}
+			float fadeTime = GameObject.Find("GameManager").GetComponent<SceneFader>().BeginFade(1);
+			yield return new WaitForSeconds (fadeTime);
+			SceneManager.LoadScene ("MainMenu");
+		}
+
+	
+}
