fc03bac94daa1042355268a64f67d284b4d85a12
cmenekse <canmenekse@sabanciuniv.edu>
2016-09-29 20:49:52 -0700

    Added simple leech drones

---
+++ Assets/Scripts/Enemy/LeechDrone/LeechDrone.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class LeechDrone : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	public float startFollowingWhenDistanceIs;
+	public float stopLeechingAfterDistance;
+	public float getDamageWhenCloserThan;
+	public Health health;
+	private bool _follow = true;
+	private bool _leeched = false;
+	private float _originalDrag;
+	private Rigidbody _rigidbody;
+	private Rigidbody _characterRigidbody;
+	void Start () 
+	{
+		_rigidbody = GetComponent<Rigidbody> ();
+		_characterRigidbody = Character.current.GetComponent<Rigidbody> ();
+		_originalDrag = _rigidbody.drag;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Follow ();
+
+	}
+
+	void Follow()
+	{
+		if (Character.current == null)
+		{
+			return;
+		} 
+		else
+		{
+			float distance = Vector3.Distance (Character.current.transform.position, transform.position);
+			//When the drone is far from the character
+			if (distance > startFollowingWhenDistanceIs)
+			{
+				if (_leeched)
+				{
+					_characterRigidbody.drag = _originalDrag;
+				}
+
+				//This means the drone is leeched but we are now escaped the leeching
+				if (_leeched && distance > stopLeechingAfterDistance)
+				{
+					_leeched = false;
+					_follow = false;
+
+					health.TakeDamage (500);
+
+				}
+				//It just follows normally
+				if (_follow)
+				{
+					transform.position = Vector3.MoveTowards (transform.position, Character.current.transform.position, speed * Time.deltaTime);
+				}
+			}
+			//When the drone is close to the character
+			else
+			{
+				//Drone starts to leech if it is close
+				if (_follow)
+				{
+					_leeched = true;
+
+					if (distance <= getDamageWhenCloserThan)
+					{
+						Attack ();
+
+					} 
+					else
+					{
+						_characterRigidbody.drag = _originalDrag;
+					}
+
+				} 
+			}
+
+
+		}
+	}
+
+	void Attack()
+	{
+		_characterRigidbody.drag = _characterRigidbody.drag + 2.0f * Time.deltaTime;
+		//Debug.Log ("Attacking");
+	}
+
+}
