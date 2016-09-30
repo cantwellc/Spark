63ab9d999c5109ef5240a62db952b2a57a0912f3
Handa Zhang <handazh2@gmail.com>
2016-09-30 14:35:26 -0700

    Modified MEventTrigger, now it has broad use

--- Assets/Scripts/Generic/MEventTrigger.cs
+++ Assets/Scripts/Generic/MEventTrigger.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class MEventTrigger : MonoBehaviour {
 
-    public UnityEvent onTrigger;
+    public UnityEvent triggerEnter;
+    public UnityEvent triggerStay;
+    public UnityEvent triggerExit;
 
+    public List<string> validTargetTags;
     // Use this for initialization
     void Start() {
 
@@ -13,9 +18,23 @@ public class MEventTrigger : MonoBehaviour {
 
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == "Character")
-        {
-            onTrigger.Invoke();
-        }
+        if (validTargetTags == null) return;
+        if (!validTargetTags.Contains(col.gameObject.tag)) return;
+        triggerEnter.Invoke();
     }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (validTargetTags == null) return;
+        if (!validTargetTags.Contains(col.gameObject.tag)) return;
+        triggerStay.Invoke();
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (validTargetTags == null) return;
+        if (!validTargetTags.Contains(col.gameObject.tag)) return;
+        triggerExit.Invoke();
+    }
+
 }
