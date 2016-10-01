ebcb9fba6f47ec67fff0fa23776be35e141a8e69
Chunze <cchunze@yahoo.com>
2016-10-01 11:55:17 -0700

    Added simple UI for sound setting

---
+++ Assets/Scripts/UI/MusicVolume.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MusicVolume : MonoBehaviour {
+
+    public Slider musicVolumeSlider;
+    public Text musicVolumeText;
+
+	// Use this for initialization
+	void Start () {
+        musicVolumeText.text = "Music Volume (" + musicVolumeSlider.value + ")";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        musicVolumeText.text = "Music Volume (" + musicVolumeSlider.value + ")";
+    }
+}
---
+++ Assets/Scripts/UI/test.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	void OnEnable()
+    {
+        gameObject.SetActive(false);
+    }
+}
