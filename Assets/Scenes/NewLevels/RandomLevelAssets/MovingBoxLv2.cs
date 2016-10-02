86b885d6203363b1941954cf31e030714aa203de
divyansb <divyansb@usc.edu>
2016-10-02 03:09:00 -0700

    Tweaks and changes
    Modified existing levels,
    slightly increased bullet mass value,
    Made some changes to the boss (shooting and rotation)

--- Assets/Scenes/NewLevels/RandomLevelAssets/MovingBoxLv2.cs
+++ Assets/Scenes/NewLevels/RandomLevelAssets/MovingBoxLv2.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class MovingBoxLv2 : MonoBehaviour {
 
-    private Vector3 targetPosition1, targetPosition2;
+    public GameObject target1, target2;
+    public float inTime;
+    
     private Vector3 velocity = Vector3.zero;
     private int target;
 
@@ -12,8 +14,6 @@ public class MovingBoxLv2 : MonoBehaviour {
     // Use this for initialization
     void Start () {
         target = 1;
-        targetPosition1 = transform.position - new Vector3(4.5f, 0.0f, 0.0f);
-        targetPosition2 = transform.position + new Vector3(4.5f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -24,14 +24,14 @@ public class MovingBoxLv2 : MonoBehaviour {
             //Debug.Log("moving to 2");
             if (tmr >= 2.0f)
             { target = 1; tmr = 0.0f;  Debug.Log("now target is 1"); }
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition2, ref velocity, 0.5f);
+            transform.position = Vector3.SmoothDamp(transform.position, target2.transform.position, ref velocity, inTime);
         }
         else if (target == 1)
         {
             //Debug.Log("moving to 1");
             if (tmr >= 2.0f)
             { target = 2; tmr = 0.0f; Debug.Log("now target is 2"); }
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition1, ref velocity, 0.5f);
+            transform.position = Vector3.SmoothDamp(transform.position, target1.transform.position, ref velocity, inTime);
         }
    }
 }
