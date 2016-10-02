1c60fcccb1a980d00d0fb5c7a60fe0c585c2d9a4
Chris Cantwell <ccantwel@usc.edu>
2016-10-02 13:22:43 -0700

    Added a VariableDrag component to the Character.
    Also added a velocity text to the in game UI to help with setting velocity and drag values in the game.  The text only works for level 1 at the moment.

---
+++ Assets/Scripts/Character/ShowVelocity.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowVelocity : MonoBehaviour {
+    public Text velocityText;
+    public Rigidbody rigidBody;
+
+    void Update()
+    {
+        velocityText.text = rigidBody.velocity.magnitude.ToString();
+    }	
+}
---
+++ Assets/Scripts/Character/VariableDrag.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simple component that allows a RigidBody drag to
+/// vary with velocity.  
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class VariableDrag : MonoBehaviour {
+    public float minVelocityThreshold;
+    public float minDrag; // Anything below the minVelocityThreshold will have minDrag
+    public float maxVelocityThreshold;
+    public float maxDrag; // Anything above the maxVelocityThreshold will have maxDrag
+    public AnimationCurve dragScalingCurve;
+
+    private Rigidbody _rigidBody;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate()
+    {
+        var v = _rigidBody.velocity.magnitude;
+        float drag;
+        if (v < minVelocityThreshold) drag = minDrag;
+        else if (v > maxVelocityThreshold) drag = maxDrag;
+        else
+        {
+            float ratio = (v - minVelocityThreshold) / (maxVelocityThreshold - minVelocityThreshold);
+            Debug.Log("Ratio: " + dragScalingCurve.Evaluate(ratio).ToString()); 
+            drag = dragScalingCurve.Evaluate(ratio) * (maxDrag - minDrag) + minDrag;
+        }
+
+        _rigidBody.drag = drag;
+    }
+}
