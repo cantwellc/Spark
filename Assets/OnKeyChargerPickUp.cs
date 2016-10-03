fd6f38f48cde1c331ab0817c7247161d524bcf17
Goksucan Akin <Goksucan Akin>
2016-10-02 21:49:21 -0700

    KeyCharger Model added to objective prefabs character intertaction implemented increase keycharger value in character. Turret keycharger drop implemented

---
+++ Assets/OnKeyChargerPickUp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnKeyChargerPickUp : MonoBehaviour
+{
+    public Character character; 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Character")
+        {
+            character.keyChargersPickedUp += 1;
+            DestroyObject(this.gameObject);
+        }
+    }
+}
