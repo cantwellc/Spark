8449afe17f5e8eaa8d57c53e3d9c6c79df931cd3
Goksucan Akin <Goksucan Akin>
2016-10-04 22:15:00 -0700

    Introduced new wall prefabs fuel sucking and drag increaser tresspassable walls, nonbounce and bouncy walls as prefabs

---
+++ Assets/IncreaseDragOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseDragOnCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Character")
+        {
+            other.GetComponent<Rigidbody>().drag += 0.01f;
+        }
+    }
+}
---
+++ Assets/SuckFuelOnCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuckFuelOnCollision : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Character")
+        {
+            other.GetComponent<Health>().TakeDamage(20);
+        }
+    }
+}
