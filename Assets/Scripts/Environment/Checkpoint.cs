c3245a3b2895df603a0e68c14f8cafa5af5ff013
Handa Zhang <handazh2@gmail.com>
2016-10-06 21:33:32 -0700

    Put all prefabs into resource folder

---
+++ Assets/Scripts/Environment/Checkpoint.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+    public bool isInitialCheckpoint;
+
+    bool _isActivated = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    void Awake()
+    {
+        if(isInitialCheckpoint)
+        {
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(_isActivated)
+        {
+            this.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        else
+        {
+            this.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        _isActivated = true;
+    }
+}
