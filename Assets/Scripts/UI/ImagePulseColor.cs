cb56f82e10ba454cca8e0632223df3560b1fc63f
Chris Cantwell <ccantwel@usc.edu>
2016-10-11 20:42:11 -0700

    Updated Backstory

---
+++ Assets/Scripts/UI/ImagePulseColor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImagePulseColor : MonoBehaviour {
+    public Color color1;
+    public Color color2;
+    public float pulseDuration;
+
+    private Image _image;
+    private bool _pulse;
+
+    void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void StartPulsing()
+    {
+        _pulse = true;
+    }
+
+    public void StopPulsing()
+    {
+        _pulse = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_pulse) return;
+        float lerp = Mathf.PingPong(Time.time, pulseDuration) / pulseDuration;
+        _image.color = Color.Lerp(color1, color2, lerp);
+    }
+}
---
+++ Assets/Scripts/Uncategorized/OnStartEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class OnStartEvent : MonoBehaviour {
+    public UnityEvent OnStart;
+	// Use this for initialization
+	void Start () {
+        OnStart.Invoke();
+	}
+	
+}
