a3aafb5396080b7c0759f9ed1f03a5c0392a3779
divyansb <divyansb@usc.edu>
2016-10-14 01:16:34 -0700

    TutorialLevel_V3
    Some changes still need to be made...
    The second set of crush walls need to be changed.

---
+++ Assets/Scenes/NewLevels/RandomLevelAssets/increaseWallSpeed.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class increaseWallSpeed : MonoBehaviour {
+
+    public Crush cWall;
+
+	void OnTriggerEnter()
+    {
+        cWall.speed *= 3;
+    }
+}
