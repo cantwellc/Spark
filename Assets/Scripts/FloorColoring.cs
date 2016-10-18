7117d1205010752c1d51ed9d96aecfa23b53714d
cmenekse <canmenekse@sabanciuniv.edu>
2016-10-18 05:53:50 +0300

    We cant go back in the chase level

---
+++ Assets/Scripts/FloorColoring.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorColoring : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name.Contains ("FloorTile"))
+		{
+			other.gameObject.GetComponent<Renderer> ().material.color = Color.red;
+			SuckFuelByDistance suckFuelOnDistance = other.gameObject.AddComponent<SuckFuelByDistance> () as SuckFuelByDistance;
+
+		}
+	}
+}
---
+++ Assets/Scripts/SuckFuelByDistance.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuckFuelByDistance : MonoBehaviour {
+
+	// Use this for initialization
+	private Transform _characterTransform;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Character.current != null)
+		{
+			if (Vector3.Distance (Character.current.transform.position, transform.position) <3.0f)
+			{
+				Character.current.GetComponent<CharacterHealth> ().TakeDamage (250.0f);
+			}
+		}
+	}
+
+}
