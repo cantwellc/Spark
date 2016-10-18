6cb663704835c93d8104f75628acedbdf13ae07a
cmenekse <canmenekse@sabanciuniv.edu>
2016-10-18 17:46:10 +0300

    Created Level 3

---
+++ Assets/Scripts/Generic/ActivateMultiple.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActivateMultiple : MonoBehaviour {
+	
+	public List<GameObject> objectsToActivate;
+	// Use this for initialization
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Character")
+		{
+			foreach (GameObject gameObject in objectsToActivate)
+			{
+				gameObject.SetActive (true);
+			}
+		}
+
+	}
+}
