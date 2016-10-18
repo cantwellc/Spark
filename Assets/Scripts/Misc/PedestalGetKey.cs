8f1c2ce1861b3511b95bf502d6f1d87ddb252bef
Chris Cantwell <ccantwel@usc.edu>
2016-10-17 22:10:17 -0700

    Merge branch 'chris' into HEAD
    Conflicts:
    	Assets/Scenes/NewLevels/tutorialLevels/TutorialLevel_V2.unity

---
+++ Assets/Scripts/Misc/PedestalGetKey.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+using System.Collections;
+using System;
+
+public class PedestalGetKey : MonoBehaviour {
+    public UnityEvent OnKeyFinalPositionReached;
+
+    public float toStageSpeed;
+    public Transform stageTarget;
+
+    public float toFinalSpeed;
+    public Transform finalTarget;
+
+
+    private bool _moveToStage;
+    private bool _moveToFinal;
+    private Transform _key;
+    private float _timeToStage;
+    private float _toStageStartTime;
+    private float _toFinalStartTime;
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Key") return;
+        _key = other.gameObject.transform;
+        _key.transform.SetParent(finalTarget, true);
+        CalcTimeToStage();
+        MoveKeyToStage();
+    }
+
+    private void CalcTimeToStage()
+    {
+        _timeToStage = Vector3.Distance(_key.position, stageTarget.position);
+    }
+
+    void MoveKeyToStage()
+    {
+        _moveToFinal = false;
+        _moveToStage = true;
+        _toStageStartTime = Time.time;
+    }
+
+    void MoveKeyToFinal()
+    {
+        _moveToStage = false;
+        _moveToFinal = true;
+        _toFinalStartTime = Time.time;
+    }
+
+    private void StopMoveFinal()
+    {
+        _moveToFinal = false;
+        _moveToStage = false;
+        OnKeyFinalPositionReached.Invoke();
+    }
+
+    void Update()
+    {
+        if (_moveToStage)
+        {
+            var step = toStageSpeed * Time.deltaTime;
+            var ratio = (Time.time - _toStageStartTime) / _timeToStage;
+            _key.position = Vector3.MoveTowards(_key.position, stageTarget.position, step);
+            _key.rotation = Quaternion.Lerp(_key.rotation, stageTarget.rotation, ratio);
+            if (_key.position == stageTarget.position) MoveKeyToFinal();
+        }
+        else if (_moveToFinal)
+        {
+            var step = toFinalSpeed * Time.deltaTime;
+            _key.position = Vector3.MoveTowards(_key.position, finalTarget.position, step);
+//            _key.rotation = Quaternion.RotateTowards(_key.rotation, finalTarget.rotation, step);
+            if (_key.position == finalTarget.position) StopMoveFinal();
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/Misc/SpawnKey.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System;
+
+public class SpawnKey : MonoBehaviour {
+    public UnityEvent OnSpawnKey;
+    public UnityEvent OnSpawnKeyFailed;
+
+    public GameObject key;
+    public bool keyCollected;
+
+	void OnEnable()
+    {
+        EventManager.StartListening(EventManager.Events.CHARACTER_ENTERED_KEY_PEDESTAL_TRIGGER, Spawn);
+        EventManager.StartListening(EventManager.Events.KEY_COLLECTED, KeyCollected);
+    }
+
+    private void KeyCollected()
+    {
+        keyCollected = true;
+    }
+
+    private void Spawn()
+    {
+        if (!keyCollected) OnSpawnKeyFailed.Invoke();
+        else {
+            key.SetActive(true);
+            OnSpawnKey.Invoke();
+        }
+    }
+}
