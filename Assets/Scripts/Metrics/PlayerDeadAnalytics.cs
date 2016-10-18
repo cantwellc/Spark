3d11c5ad41a642c0a5d85574cd25f92a13349d5c
Handa Zhang <handazh2@gmail.com>
2016-10-18 00:36:21 -0700

    Added metrics

---
+++ Assets/Scripts/Metrics/PlayerDeadAnalytics.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+using System;
+
+public class PlayerDeadAnalytics : MonoBehaviour {
+
+	public void OnPlayerDeath()
+    {
+        GameObject obj = this.gameObject;
+
+        Analytics.CustomEvent("PlayerDead", new Dictionary<string, object>
+        {
+            { "position", obj.transform.position }
+          });
+        print("event");
+    }
+}
