f557745409209a5f5ec904487d9fafa359234cf6
cmenekse <canmenekse@sabanciuniv.edu>
2016-10-19 15:49:07 +0300

    Added very primitive Attach drone

--- Assets/Scripts/Uncategorized/LeechDroneAttach.cs
+++ Assets/Scripts/Uncategorized/LeechDroneAttach.cs
@@ -5,6 +5,7 @@ public class LeechDroneAttach : MonoBehaviour {
 
 	private float _speed = 2;
 	public Transform droneTransform;
+	private float _angleOfAttach; 
 	// Use this for initialization
 	void Start () {
 	
@@ -15,33 +16,40 @@ public class LeechDroneAttach : MonoBehaviour {
 	
 	}
 
-	void OnTriggerEnter()
+	void OnTriggerEnter(Collider other)
 	{
-		float cx = Character.current.transform.position.x;
-		float cz = Character.current.transform.position.z;
-		float r = 1.0f;
-		float eulerAngleY = Character.current.transform.rotation.eulerAngles.y +180;
-		float angleInDegrees =  (eulerAngleY > 180.0f) ? eulerAngleY - 360.0f : eulerAngleY;
-		float a = angleInDegrees * Mathf.Deg2Rad;
-
-		Vector3 attachTo = new Vector3(cx - r * Mathf.Cos (a),1.0f, cz + r * Mathf.Sin (a));
-		droneTransform.position = Vector3.MoveTowards (droneTransform.position, attachTo, _speed * Time.deltaTime);
-	
-	}
-
-	void OnTriggerStay()
-	{
-		if (Vector3.Distance (transform.position, Character.current.transform.position) > 0.2f)
+		if (other.gameObject.tag == "Character")
 		{
 			float cx = Character.current.transform.position.x;
 			float cz = Character.current.transform.position.z;
 			float r = 1.0f;
-			float eulerAngleY = Character.current.transform.rotation.eulerAngles.y + 180;
-			float angleInDegrees = (eulerAngleY > 180.0f) ? eulerAngleY - 360.0f : eulerAngleY;
+			_angleOfAttach = Character.current.transform.rotation.eulerAngles.y + 90 + Random.Range (20, 190);
+			Debug.Log (_angleOfAttach);
+			float angleInDegrees = (_angleOfAttach > 180.0f) ? _angleOfAttach - 360.0f : _angleOfAttach;
 			float a = angleInDegrees * Mathf.Deg2Rad;
 
 			Vector3 attachTo = new Vector3 (cx - r * Mathf.Cos (a), 1.0f, cz + r * Mathf.Sin (a));
 			droneTransform.position = Vector3.MoveTowards (droneTransform.position, attachTo, _speed * Time.deltaTime);
+		}
+	
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Character")
+		{
+			if (Vector3.Distance (transform.position, Character.current.transform.position) > 0.2f)
+			{
+				float cx = Character.current.transform.position.x;
+				float cz = Character.current.transform.position.z;
+				float r = 1.0f;
+
+				float angleInDegrees = (_angleOfAttach > 180.0f) ? _angleOfAttach - 360.0f : _angleOfAttach;
+				float a = angleInDegrees * Mathf.Deg2Rad;
+
+				Vector3 attachTo = new Vector3 (cx - r * Mathf.Cos (a), 1.0f, cz + r * Mathf.Sin (a));
+				droneTransform.position = Vector3.MoveTowards (droneTransform.position, attachTo, _speed * Time.deltaTime);
+			}
 		}
 	}
 }
