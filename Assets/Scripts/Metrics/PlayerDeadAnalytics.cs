a6a0e0d8d418b66bb12afbf39c09db1716443b8d
Handa Zhang <handazh2@gmail.com>
2016-10-18 21:29:50 -0700

    Updated player dead metrics

--- Assets/Scripts/Metrics/PlayerDeadAnalytics.cs
+++ Assets/Scripts/Metrics/PlayerDeadAnalytics.cs
@@ -10,7 +10,6 @@ public class PlayerDeadAnalytics : MonoBehaviour {
     {
         GameObject obj = this.gameObject;
         
-        Analytics.CustomEvent("PlayerDead", new Dictionary<string, object>
         {
             { "position", obj.transform.position },
             { "level", SceneManager.GetActiveScene().name }
