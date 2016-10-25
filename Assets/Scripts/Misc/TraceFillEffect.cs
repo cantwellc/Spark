0314249aad1d003950338aa817bc8a760dcc03cf
Chris Cantwell <ccantwel@usc.edu>
2016-10-25 11:43:10 -0700

    Added a fill effect to GameStory

---
+++ Assets/Scripts/Misc/TraceFillEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TraceFillEffect : MonoBehaviour {
+    public UIImageFill imageFill1;
+    public UIImageFill imageFill2;
+    public float totalFillTime;
+    public float fillDelay;
+
+	public void StartEffect()
+    {
+        StartCoroutine(CoStartEffect());
+    }
+
+    private IEnumerator CoStartEffect()
+    {
+        imageFill1.StartFill(totalFillTime);
+        yield return new WaitForSeconds(fillDelay);
+        imageFill2.StartFill(totalFillTime);
+    }
+}
---
+++ Assets/Scripts/Misc/UIImageFill.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIImageFill : MonoBehaviour {
+    private Image _image;
+    private float _startTime;
+    private float _timeToFill;
+    private bool _doFill;
+     
+    void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+	public void StartFill(float timeToFill)
+    {
+        _startTime = Time.time;
+        _timeToFill = timeToFill;
+        _doFill = true;
+    }
+	// Update is called once per frame
+	void Update () {
+        if (!_doFill) return;
+        var ratio = (Time.time - _startTime) / _timeToFill;
+        _image.fillAmount = ratio;
+	}
+}
