a488a6f4e76c1b70f3d85a4b48f185dbc626c468
cmenekse <canmenekse@sabanciuniv.edu>
2016-10-26 16:57:39 -0700

    Second set of walls in the Level 1 works

---
+++ Assets/Scripts/Environment/TriggerCrush.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class TriggerCrush : MonoBehaviour {
+
+	public GameObject wall;
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Character")
+		{
+			wall.GetComponent<Crush> ().StartCrushing();
+
+
+		}
+	}
+}
