29322b2e38a0569e2a1e01b6603b8cfb824b2970
cmenekse <canmenekse@sabanciuniv.edu>
2016-10-28 10:07:45 -0700

    Plasma Burst VFX

---
+++ Assets/DrawLaser2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLaser2 : MonoBehaviour {
+
+	private LineRenderer _lineRenderer;
+	public Transform start;
+	public Transform destination;
+	// Use this for initialization
+	void Start () 
+	{
+		_lineRenderer = GetComponent<LineRenderer> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		_lineRenderer.SetPosition (0, start.position);
+		_lineRenderer.SetPosition (1, destination.position);
+		_lineRenderer.SetWidth (.2f, .2f);
+
+	}
+}
