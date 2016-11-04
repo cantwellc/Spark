727a8d748de0a2d436c6ef23ad97eb5e5d8d7d89
Handa Zhang <handazh2@gmail.com>
2016-11-04 05:33:07 -0700

    Added wall generator

---
+++ Assets/Scripts/LevelBuilding/MouseCapturePointsEditor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MouseCapturePoints))]
+public class MouseCapturePointsEditor : Editor {
+
+    MouseCapturePoints point;
+
+    SerializedProperty toClear;
+    SerializedProperty y;
+
+    GameObject childPrefab;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void OnEnable()
+    {
+        point = (MouseCapturePoints)target;
+        toClear = serializedObject.FindProperty("toClear");
+        y = serializedObject.FindProperty("y");
+
+        childPrefab = (GameObject) Resources.Load("Prefabs/UI/CubeGizmos");
+    }
+
+    void OnSceneGUI()
+    {
+        Event e = Event.current;
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 1)
+        {
+            Vector3 planePos = point.transform.position;
+            planePos.y = point.y;
+            Plane wallPlane = new Plane(Vector3.up, planePos);
+            Vector2 mousePos = Event.current.mousePosition;
+            mousePos.y = Camera.current.pixelHeight - mousePos.y;
+            Vector3 position = Camera.current.ScreenPointToRay(mousePos).origin;
+
+            Ray ray = Camera.current.ScreenPointToRay(mousePos);
+            float hitdist = 0.0f;
+            // If the ray is parallel to the plane, Raycast will return false.
+            if (wallPlane.Raycast(ray, out hitdist))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+                //Debug.DrawRay(Camera.current.transform.position, targetPoint, Color.red);
+                GameObject obj = (GameObject)Instantiate(childPrefab);
+                obj.transform.position = targetPoint;
+                obj.transform.parent = point.transform;
+
+            }
+            
+            
+        }
+        switch (e.type)
+        {
+            case EventType.keyDown:
+                {
+                    if (Event.current.keyCode == (KeyCode.A))
+                    {
+                        
+                    }
+                    break;
+                }
+
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //serializedObject.Update();
+        //EditorGUILayout.PropertyField(lookAtPoint);
+        //EditorGUILayout.LabelField("(Below this object)");
+        EditorGUILayout.PropertyField(toClear);
+         EditorGUILayout.PropertyField(y);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/LevelBuilding/WallMeshGenerator.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class WallMeshGenerator : MonoBehaviour {
+
+    public GameObject WallPoints;
+
+    public bool createMesh;
+    public bool clearMesh;
+
+    public float wallHeight;
+    public float wallWidth;
+
+    Vector3[] vertices;
+    Vector2[] uvs;
+    Vector2[] normals;
+    int[] triangles;
+
+    Mesh mesh;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void PushTriangles(int index, int v1, int v2, int v3)
+    {
+        triangles[index] = v1;
+        triangles[index+1] = v2;
+        triangles[index+2] = v3;
+    }
+
+    void PushTriangles(int index, int v1, int v2, int v3,int v4)
+    {
+        triangles[index] = v1 + index/36*4;
+        triangles[index + 1] = v2 + index / 36 * 4;
+        triangles[index + 2] = v3 + index / 36 * 4;
+        triangles[index+3] = v3 + index / 36 * 4;
+        triangles[index + 4] = v2 + index / 36 * 4;
+        triangles[index + 5] = v4 + index / 36 * 4;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if(createMesh == true)
+        {
+            createMesh = false;
+            if(WallPoints != null)
+            {
+                mesh = new Mesh();
+
+                int childCount = WallPoints.transform.childCount;
+                vertices = new Vector3[childCount * 4];
+                uvs = new Vector2[childCount * 4];
+                triangles = new int[childCount * 6 * 2*3];
+
+                int ind ;
+                Vector3 pos1;
+                Vector3 pos2;
+                for (int a=0;a<WallPoints.transform.childCount;++a)
+                {
+                    Transform [] child = new Transform[2];
+                    child[0] = WallPoints.transform.GetChild(a);
+                    if(a < WallPoints.transform.childCount-1)
+                        child[1] = WallPoints.transform.GetChild(a+1);
+                    else
+                    {
+                        child[0] = WallPoints.transform.GetChild(a);
+                        child[1] = WallPoints.transform.GetChild(a-1);
+                    }
+                    Vector3 wallDir = child[1].position - child[0].position;
+                    if (a == WallPoints.transform.childCount - 1)
+                        wallDir = -wallDir;
+                    Vector3 wallWidthDir = Vector3.Cross(wallDir.normalized, Vector3.up);
+                    pos1 = child[0].position;
+                    pos2 = child[1].position;
+                    vertices[4*a] = pos1;
+                    vertices[4 * a+1] = pos1;
+                    vertices[4 * a + 1].y += wallHeight;
+                    vertices[4 * a+2] = pos1;
+                    vertices[4 * a + 2] += wallWidth*wallWidthDir;
+                    vertices[4 * a+3] = pos1;
+                    vertices[4 * a + 3].y += wallHeight;
+                    vertices[4 * a + 3] += wallWidth*wallWidthDir;
+
+                    if(a%2 == 0)
+                    {
+                        uvs[4 * a] = new Vector2(0.0f, 0.0f);
+                        uvs[4 * a+1] = new Vector2(0.0f, 1.0f);
+                        uvs[4 * a+2] = new Vector2(1.0f, 0.0f);
+                        uvs[4 * a+3] = new Vector2(1.0f, 1.0f);
+                    }
+                    else
+                    {
+                        uvs[4 * a] = new Vector2(1.0f, 0.0f);
+                        uvs[4 * a + 1] = new Vector2(1.0f, 1.0f);
+                        uvs[4 * a + 2] = new Vector2(0.0f, 0.0f);
+                        uvs[4 * a + 3] = new Vector2(0.0f, 1.0f);
+                    }
+                    
+                }
+                for (int a = 0; a < WallPoints.transform.childCount-1; ++a)
+                {
+                    ind = 36 * a;
+                    PushTriangles(ind, 0 , 2 , 1, 3);
+                    PushTriangles(ind + 6, 0  , 1  , 4  , 5  );
+                    PushTriangles(ind + 12, 1  , 3  , 5  , 7  );
+                    PushTriangles(ind + 18, 2  , 6  , 3  , 7  );
+                    PushTriangles(ind + 24, 0  , 4  , 2  , 6  );
+                    PushTriangles(ind + 30, 4  , 5  , 6  , 7  );
+                }
+
+                mesh.vertices = vertices;
+                mesh.uv = uvs;
+                mesh.triangles = triangles;
+
+                mesh.name = "Generated mesh";
+                GetComponent<MeshFilter>().mesh = mesh;
+            }
+            
+        }
+        if(clearMesh)
+        {
+            clearMesh = false;
+            GetComponent<MeshFilter>().mesh.Clear();
+        }
+	}
+}
