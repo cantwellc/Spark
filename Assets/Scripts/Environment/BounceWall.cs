b3776fc89c5389775520a59512e699fddab06195
Handa Zhang <handazh2@gmail.com>
2016-11-05 18:56:05 -0700

    Added level 3

--- Assets/Scripts/Environment/BounceWall.cs
+++ Assets/Scripts/Environment/BounceWall.cs
@@ -19,9 +19,11 @@ public class BounceWall : MonoBehaviour {
         {
             print(col.relativeVelocity);
             float angle = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
-            Vector3 dir = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+            Vector3 dir = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle));
+            print(dir);
             Vector3 velocity = col.relativeVelocity;
             velocity = 2.0f * (Vector3.Dot(dir ,velocity)) * dir - velocity;
+            print(velocity);
             col.rigidbody.velocity = -velocity*bounceBoost;
             //col.rigidbody.AddForce(dir * bounceForce, ForceMode.Impulse);
         }
