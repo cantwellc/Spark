89d608efbe81e842d222cb619940e1c98c7d6ed0
Chris Cantwell <ccantwel@usc.edu>
2016-11-09 16:47:09 -0800

    Updated Character.prefab fuel

---
+++ Assets/Scripts/Misc/RotateWithFuel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateWithFuel : MonoBehaviour {
+    public bool rotateClockwise;
+    public bool rotateX;
+    public bool rotateY;
+    public bool rotateZ;
+    public FuelReservoir fuelReservoir;
+	
+	// Update is called once per frame
+	void Update () {
+        var r = (1 -(fuelReservoir.fuelCount / fuelReservoir.maxFuelCount)) * 180;
+        if (rotateClockwise) r = -r;
+        var rX = rotateX ? r : 0.0f;
+        var rY = rotateY ? r : 0.0f;
+        var rZ = rotateZ ? r : 0.0f;
+        transform.localEulerAngles = new Vector3(rX, rY, rZ);
+	}
+}
