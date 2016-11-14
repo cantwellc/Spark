7fa424fc20b9822625f98f549641f673e7de4d80
cmenekse <canmenekse@sabanciuniv.edu>
2016-11-13 18:05:43 -0800

    Material_files

---
+++ Assets/VFX_Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VFX_Player : MonoBehaviour {
+
+	public ParticleSystem particleEffect;
+
+	// Update is called once per frame
+	public void PlayEffect()
+	{
+		particleEffect.Play ();
+		AudioManager.instance.Play ("burstFire");
+	}
+}
