71d9e4189d14ae279e2d373f123bcb7a12391339
cmenekse <canmenekse@sabanciuniv.edu>
2016-11-15 11:09:31 -0800

    Added transition to level 8

--- Assets/StopCharacter.cs
+++ Assets/StopCharacter.cs
@@ -12,7 +12,7 @@ public class StopCharacter : MonoBehaviour {
 			GameObject initialCheckpoint = GameObject.Find ("InitialCheckpointWithAnimation");
 			initialCheckpoint.GetComponent<CharacterAnimationPlay> ().ReEnableInput ();
 			Vector3 normalizedVelocity = Character.current.GetComponent<Rigidbody> ().velocity.normalized;
-			Vector3 forceToApply = normalizedVelocity * -20;
+			Vector3 forceToApply = normalizedVelocity * -10;
 			Character.current.GetComponent<Rigidbody> ().AddForce(forceToApply, ForceMode.Impulse);
 			Debug.Log ("Running this");
 
