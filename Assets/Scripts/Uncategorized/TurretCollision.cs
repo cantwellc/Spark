4e899bbd4aa6b47db75d908021a87fa53b605ffd
cmenekse <canmenekse@sabanciuniv.edu>
2016-11-16 10:42:12 -0800

    Turret collision does more damage, more likely to crash into a turret in level 2

--- Assets/Scripts/Uncategorized/TurretCollision.cs
+++ Assets/Scripts/Uncategorized/TurretCollision.cs
@@ -23,7 +23,7 @@ public class TurretCollision : MonoBehaviour
         {
             Rigidbody _rbCharacter = col.gameObject.GetComponent<Rigidbody>();
             float damageTaken = (2 * _rbCharacter.mass * _rbCharacter.velocity.magnitude * _rbCharacter.velocity.magnitude / _rbTurret.mass);
-			damageTaken = damageTaken / 170;
+			damageTaken = damageTaken / 100;
 			collisionTrigger.Invoke(damageTaken);
         }
 
