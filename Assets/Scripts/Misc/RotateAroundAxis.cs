0b1480c6004ac394e0d2e1ce3d31ed16d6a79abc
Chris Cantwell <ccantwel@usc.edu>
2016-11-19 21:32:04 -0800

    Added rotating_turret_base to level 7 and adjusted walls

---
+++ Assets/Scripts/Misc/RotateAroundAxis.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAroundAxis : MonoBehaviour {
+    public enum RotationAxis
+    {
+        x,y,z
+    }
+    public RotationAxis axis;
+    public float rotateSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        float rotation = Time.deltaTime * rotateSpeed;
+        float x = axis == RotationAxis.x ? rotation : 0.0f;
+        float y = axis == RotationAxis.y ? rotation : 0.0f;
+        float z = axis == RotationAxis.z ? rotation : 0.0f;
+        transform.Rotate(new Vector3(x, y, z));
+    }
+}
