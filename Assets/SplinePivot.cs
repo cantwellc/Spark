42dcd3986791260384f7ad3d2216ca895543d21d
Chris Cantwell <ccantwel@usc.edu>
2016-11-20 08:22:31 -0800

    Made SplinePivot Gizmo selectable

--- Assets/SplinePivot.cs
+++ Assets/SplinePivot.cs
@@ -1,11 +1,31 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
 public class SplinePivot : MonoBehaviour {
 
-	void OnDrawGizmos()
+    public Vector3 position
+    {
+        get
+        {
+            return transform.position;
+        }
+    }
+
+	//void OnDrawGizmos()
+ //   {
+ //       Gizmos.color = Color.yellow;
+ //       Gizmos.DrawWireSphere(transform.position, 0.25f);
+ //   }
+
+}
+
+public class SplinePivotGizmoDrawer
+{
+    [DrawGizmo(GizmoType.Pickable | GizmoType.InSelectionHierarchy | GizmoType.NotInSelectionHierarchy)]
+    private static void DrawGizmo( SplinePivot sp, GizmoType gizmoType)
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, 0.25f);
+        Gizmos.DrawWireSphere(sp.position, 0.25f);
     }
 }
