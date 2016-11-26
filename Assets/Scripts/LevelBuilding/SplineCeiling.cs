5a5fa3dc9cc5bc6526d4cfe4c6e17356e96ae04f
Chris Cantwell <ccantwel@usc.edu>
2016-11-26 13:32:48 -0800

    Updated SplineCeiling.cs to use new Delaunay triangulation from Triangle-net for unity.  https://github.com/parahunter/triangle-net-for-unity.  MIT license.

--- Assets/Scripts/LevelBuilding/SplineCeiling.cs
+++ Assets/Scripts/LevelBuilding/SplineCeiling.cs
@@ -1,13 +1,17 @@
 ﻿#if UNITY_EDITOR
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using System;
+using System.Linq;
+using TriangleNet.Geometry;
 
 [ExecuteInEditMode]
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshFilter))]
 public class SplineCeiling : MonoBehaviour
 {
+    public bool invertNormals;
+
     [HideInInspector]
     private int _ownerID;
 
@@ -48,6 +52,7 @@ public class SplineCeiling : MonoBehaviour
             return _meshCollider;
         }
     }
+
     //    public int quadCount = 1;
 
     //    public float tangentSmoothFactor;
@@ -56,24 +61,76 @@ public class SplineCeiling : MonoBehaviour
     public void DrawMesh()
     {
         var verts = wall.ceilingVertices;
-        Array.Reverse(verts);
-        var tr = new Triangulator(verts);
-        var triangles = tr.Triangulate();
-        for(int i=0; i<triangles.Length; i += 3)
+        if(invertNormals) Array.Reverse(verts);
+
+        
+        InputGeometry geometry = new InputGeometry();
+        List<Point> border = new List<Point>();
+        foreach(var vert in verts)
         {
-            int temp = triangles[i + 0];
-            triangles[i + 0] = triangles[i + 1];
-            triangles[i + 1] = temp;
+            border.Add(new Point(vert.x, vert.z));
+//            geometry.AddPoint(vert.x, vert.z);
+        }
+
+        geometry.AddRing(border);
+        
+//        geometry.AddRegion()
+        float yOffset = verts[0].y;
+        TriangleNet.Mesh tMesh = new TriangleNet.Mesh();
+        tMesh.Triangulate(geometry);
+
+        List<Vector3> vertices = new List<Vector3>(tMesh.triangles.Count * 3);
+        List<int> triangles = new List<int>(tMesh.triangles.Count * 3);
+        int tIndex = 0;
+
+        foreach(var pair in tMesh.triangles)
+        {
+            var triangle = pair.Value;
+            var v0 = triangle.GetVertex(0);
+            var v1 = triangle.GetVertex(1);
+            var v2 = triangle.GetVertex(2);
+
+            var p0 = new Vector3(v0.x, yOffset, v0.y);
+            var p1 = new Vector3(v1.x, yOffset, v1.y);
+            var p2 = new Vector3(v2.x, yOffset, v2.y);
+
+            vertices.Add(p0);
+            vertices.Add(p1);
+            vertices.Add(p2);
+
+            triangles.Add(tIndex + 2);
+            triangles.Add(tIndex + 1);
+            triangles.Add(tIndex + 0);
+
+            tIndex += 3;
         }
 
         mesh.Clear();
-        mesh.vertices = verts;
-        mesh.triangles = triangles;
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
-       
+
 
         meshCollider.sharedMesh = mesh;
+        //var tr = new Triangulator(verts);
+        //var triangles = tr.Triangulate();
+        //for(int i=0; i<triangles.Length; i += 3)
+        //{
+        //    int temp = triangles[i + 0];
+        //    triangles[i + 0] = triangles[i + 1];
+        //    triangles[i + 1] = temp;
+        //}
+
+        //mesh.Clear();
+        //mesh.vertices = verts;
+        //mesh.triangles = triangles;
+        //mesh.RecalculateNormals();
+        //mesh.RecalculateBounds();
+
+
+        //meshCollider.sharedMesh = mesh;
+
     }
 
     public void ClearMesh()
