fdadbe0e0e3cac29f2d4dee36f6aa6dd269284ac
cmenekse <canmenekse@sabanciuniv.edu>
2016-11-25 20:32:46 -0800

    Added progress save on game

---
+++ Assets/ContinueScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ContinueScript : MonoBehaviour {
+
+	public void LoadLastLevel()
+	{
+		if (!PlayerPrefs.HasKey ("LastLevel"))
+		{
+			//Make the btn not clickable
+		} 
+		else
+		{
+			string sceneName  = PlayerPrefs.GetString ("LastLevel");
+			StartCoroutine(LoadScene(sceneName));
+		}
+
+	}
+
+	IEnumerator LoadScene(string sceneName)
+	{
+		float fadeTime = GameObject.Find("GameManager").GetComponent<SceneFader>().BeginFade(1);
+		yield return new WaitForSeconds(fadeTime);
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void StartPlay()
+	{
+		EventManager.TriggerEvent(EventManager.Events.MAIN_MENU_START);
+	}
+}
