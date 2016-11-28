1df90043b474aa22b0aba5707e16b3d893ea7099
cmenekse <canmenekse@sabanciuniv.edu>
2016-11-27 19:35:33 -0800

    Ending scene and credit scene added

---
+++ Assets/Scripts/Environment/EndingScene/TriggerOtherText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerOtherText : MonoBehaviour {
+
+	public GameObject next;
+	public float secondsAfterInitializing = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Invoke ("Activate", secondsAfterInitializing);
+	}
+	
+	void Activate()
+	{
+		if(next!=null)
+		next.SetActive (true);
+	}
+}
