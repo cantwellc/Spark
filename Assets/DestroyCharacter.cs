d5d8d346229f13cd37c6264549a64ea1c1f41d4e
cmenekse <canmenekse@sabanciuniv.edu>
2016-11-28 10:49:43 -0800

    Character appearing in the main menu fixed

---
+++ Assets/DestroyCharacter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCharacter : MonoBehaviour {
+
+	private bool _destroyed = false;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GameObject character = GameObject.Find ("Character(Clone)") as GameObject;
+		if (character != null)
+		{
+			_destroyed = true;
+			character.SetActive (false);
+		}
+
+	}
+}
