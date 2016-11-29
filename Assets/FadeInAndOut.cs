b03381fa49e28e44af297688b6b6c64af144f045
cmenekse <canmenekse@sabanciuniv.edu>
2016-11-29 11:20:40 -0800

    Ending text fades in and out

---
+++ Assets/FadeInAndOut.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class FadeInAndOut : MonoBehaviour {
+
+	public float fadeInSeconds = 5.0f;
+	public float fadeOutSeconds = 1.0f;
+	public float intervalBetween = 1.0f;
+	public Text text;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		text.canvasRenderer.SetAlpha( 0.01f );
+		//Color color = text.GetComponent<Text> ().color;
+		//Color newColor = new Color (1, 1, 1, 0);
+		//text.GetComponent<Text> ().color = newColor;
+		startFadeIn ();
+	}
+	
+	// Update is called once per frame
+
+
+	void startFadeOut()
+	{
+		text.CrossFadeAlpha(0.0f, fadeOutSeconds, false);
+	}
+
+	void startFadeIn()
+	{
+		text.CrossFadeAlpha(1.0f, fadeInSeconds, false);
+		Invoke ("startFadeOut", intervalBetween);
+	}
+
+
+}
